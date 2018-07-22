3102d092b1a052bcc67f9075e17862665ef3a39f
Douglas Coelho <douglas@mouseweb.com.br>
2018-07-22 03:37:43 -0300

    padrao DTO para retornar user

--- src/main/java/br/com/mouseweb/domain/resources/UserResource.java
+++ src/main/java/br/com/mouseweb/domain/resources/UserResource.java
@@ -1,6 +1,7 @@
 package br.com.mouseweb.domain.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.mouseweb.domain.User;
+import br.com.mouseweb.dto.UserDTO;
 import br.com.mouseweb.services.UserService;
 
 @RestController
@@ -19,9 +21,10 @@ public class UserResource {
 	private UserService service;
 
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 
 }
---
+++ src/main/java/br/com/mouseweb/dto/UserDTO.java
@@ -0,0 +1,48 @@
+package br.com.mouseweb.dto;
+
+import java.io.Serializable;
+
+import br.com.mouseweb.domain.User;
+
+public class UserDTO implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private String id;
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	private String name;
+	private String email;
+	
+	public UserDTO() {
+		
+	}
+	
+	public UserDTO(User obj) {
+		id = obj.getId();
+		name = obj.getName();
+		email = obj.getEmail();
+	}
+}
