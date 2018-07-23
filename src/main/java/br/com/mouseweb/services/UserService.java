df33f9f970cb14ab9c993bca7c3a81a3faf8d0e7
Douglas Coelho <douglas@mouseweb.com.br>
2018-07-23 00:14:29 -0300

    Atualizacao de usuario com PUT

--- src/main/java/br/com/mouseweb/services/UserService.java
+++ src/main/java/br/com/mouseweb/services/UserService.java
@@ -26,16 +26,27 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-	
+
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
-	
+
 	public void delete(String id) {
 		findById(id);
 		repo.deleteById(id);
 	}
-	
+
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
