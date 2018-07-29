b69b05b242151a83dbccac1592cda3b2a5b771f9
Douglas Coelho <douglas@mouseweb.com.br>
2018-07-28 22:38:43 -0300

    Consulta com varios criterio

--- src/main/java/br/com/mouseweb/repository/PostRepository.java
+++ src/main/java/br/com/mouseweb/repository/PostRepository.java
@@ -1,7 +1,7 @@
 package br.com.mouseweb.repository;
 
+import java.util.Date;
 import java.util.List;
-
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -14,4 +14,12 @@ public interface PostRepository extends MongoRepository<Post, String>{
 	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ $and: [ "
+				+ "{ date: {$gte: ?1} }, { date: { $lte: ?2} } , "
+			+ "{ $or: [ "
+				+ "{ 'title': { $regex: ?0, $options: 'i' } }, "
+				+ "{ 'body': { $regex: ?0, $options: 'i' } }, "
+				+ "{ 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }") 
+	List<Post> fullSearch(String text, Date minDate, Date maxDate); 
 }
--- src/main/java/br/com/mouseweb/services/PostService.java
+++ src/main/java/br/com/mouseweb/services/PostService.java
@@ -1,9 +1,12 @@
 package br.com.mouseweb.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import br.com.mouseweb.domain.Post;
 import br.com.mouseweb.repository.PostRepository;
 import br.com.mouseweb.services.exception.ObjectNotFoundException;
@@ -18,10 +21,16 @@ public class PostService {
 		Optional<Post> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-	
-	public List<Post> findByTitle(String text){
+
+	public List<Post> findByTitle(String text) {
 		return repo.searchTitle(text);
-		//return repo.findByTitleContainingIgnoreCase(text);
+		// return repo.findByTitleContainingIgnoreCase(text);
+	}
+
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
+
 	}
 
 }
